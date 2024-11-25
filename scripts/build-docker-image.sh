2dcfa50f5fd1bc3b6a8dd0c2568a7966968d2e0e
root <root@localhost>
2024-11-25 13:00:54 +0000

    Added docker build scripts

---
+++ scripts/build-docker-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Expects tagname as parameter \$1."
+  exit 1
+fi
+
+TAG=$1
+
+SCRIPT_PATH=$(dirname $0)
+DOCKER_PATH=$(realpath "$SCRIPT_PATH/../")
+DOCKER_FILE="Dockerfile"
+
+echo Building, tag is: $TAG
+echo Dockerfile Path: $DOCKER_PATH
+echo Dockerfile Name: $DOCKER_FILE
+
+docker build \
+  --target run \
+  --tag $TAG \
+  -f $DOCKER_FILE \
+  $DOCKER_PATH
